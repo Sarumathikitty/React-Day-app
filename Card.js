cfe12cfb6ce87263fb104ad6484152ae3dee8359
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-07-06 09:11:51 +0530

    Create Card.js

---
+++ Card.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+const Card = ({ day, createTask, toggleTask,Deletetask,deleteday }) => {
+  const [task, setTask] = useState("");
+
+  return (
+    <div className="each-day">
+      <h2>{day.title}</h2>
+
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button
+        onClick={(_) => {
+          createTask(task);
+          setTask("");
+        }}
+      >
+        Create Task
+      </button>
+
+      <div className="task-wrapper">
+        {day.tasks.map((task, taskIndex) => {
+          return (
+            <div
+              className="each-task"
+              key={taskIndex}
+              onClick={() => toggleTask(taskIndex)}
+            >
+              <h3 className={task.completed === true ? "line" : ""}>
+                {task.title}
+                <a  onClick={()=> Deletetask(taskIndex)}><span><button>D</button></span></a>
+              </h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
